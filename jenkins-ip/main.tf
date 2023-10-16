b5777417e0608cb100fd74276fbb259381a55ef9
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-16 22:45:04 +0530

    Ansible

--- jenkins-ip/main.tf
+++ jenkins-ip/main.tf
@@ -18,3 +18,15 @@ resource "aws_route53_record" "jenkins" {
    records = [data.aws_instance.instance.public_ip]
    ttl = 10
 }
+
+data "aws_instance" "artifactory" {
+  instance_id = "i-08a4d4e1606062ca7"
+}
+
+resource "aws_route53_record" "artifactory" {
+   name = "artifactory.ayeeshadevops75.online"
+   type = "A"
+   zone_id = "Z0073724138YO075ETFE9"
+   records = [data.aws_instance.artifactory.public_ip]
+   ttl = 10
+}
