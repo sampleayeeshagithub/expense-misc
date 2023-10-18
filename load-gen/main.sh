bd88289ba6f8f0c66f586eacf9d0509cc1498a3d
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-18 16:01:36 +0530

    Ansible

--- load-gen/main.sh
+++ load-gen/main.sh
@@ -1,4 +1,4 @@
 requests=$1
 
-curl https://random-word-api.herokuapp.com/word?number=10 -o /tmp/words
+curl -s "https://random-word-api.herokuapp.com/word?number=$requests" -o /tmp/words
 cat /tmp/words
