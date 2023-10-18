6d627eba31ed9ecf72b441a020c25c0f9b7d2af0
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-18 16:19:57 +0530

    Ansible

--- load-gen/main.sh
+++ load-gen/main.sh
@@ -1,4 +1,4 @@
 requests=$1
 
-curl -s "https://random-word-api.herokuapp.com/word?number=$requests" -o /tmp/words
+curl -s "https://random-word-api.herokuapp.com/word?number=$requests" | sed -e "s/\[//" -e 's/\]//' | sed -e 's/,/ /g' -e 's/"//g' >/tmp/words
 cat /tmp/words
