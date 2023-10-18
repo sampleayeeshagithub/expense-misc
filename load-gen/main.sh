b9f27718b14823eda098285f80cb7b0716c89bb9
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-18 20:09:16 +0530

    Ansible

--- load-gen/main.sh
+++ load-gen/main.sh
@@ -1,8 +1,9 @@
 requests=$1
+url=$2
 
 curl -s "https://random-word-api.herokuapp.com/word?number=$requests" | sed -e "s/\[//" -e 's/\]//' | sed -e 's/,/ /g' -e 's/"//g' >/tmp/words
 
 
 for word in `cat /tmp/words`; do
-  echo $word
+  curl -X POST $url/api/transaction --header "Content-Type: application/json" -d "{\"amount\":\"$RANDOM\",\"desc\":\"$word\"}"
 done
